b06f2ccd751a707603c259eb7d37a065a81c3562
alex <axmitchell202@gmail.com>
2020-04-07 19:15:26 -0400

    denormalized db

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-const Availability = require('../db/db.js');
+const db = require('../db/db.js');
 const PUBLIC_DIR = path.join(__dirname, '..', '/public');
 const app = express();
 
@@ -13,31 +13,26 @@ app.use(bodyParser.json());
 
 app.use('/:id', express.static(PUBLIC_DIR));
 
-// app.get('/:id', (req, res) => {
-//   if (!req.params.id) {
-//     res.status(400);
-//     res.end();
-//   } else {
-//     res.sendFile('index.html', { root: path.resolve(__dirname, '../public') });
-//   }
-// });
-
 app.get('/:id/reservations', (req, res) => {
   const resID = Number(req.params.id);
-
-  Availability.findOne({ where: { id: resID } })
-    .then((main) => {
-      res.status(200).send(main);
-    })
+  const restaurantQuery = db.Restaurant.findOne({ where: { id: resID } })
+  // const availabilityQuery = db.Availability.findAll({ where: { rest_id: resID }, attributes: ["time", "seats"] })
+  // Promise.all([restaurantQuery, availabilityQuery])
+  //   .then(([restRes, availRes]) => {
+  //     let restaurant = restRes.dataValues
+  //     restaurant.times = {}
+  //     availRes.forEach(res => {
+  //       let { time, seats } = res;
+  //       restaurant.times[time] = seats;
+  //     })
+  //     res.send(restaurant)
+  //   })
+  //   .catch((err) => (res.status(404).send('unable to retrieve from db: ', err)));
+  restaurantQuery 
+    .then((data) => {res.status(200).send(data)})
     .catch((err) => {
-      res.status(404).send('unable to retrieve from db: ', err);
-    });
+      res.status(500).send('unable to retrieve from db: ', err)
+    })
 });
 
 module.exports = app;
-
-
-/*
-on the main react components mount, an axios get request is made to the route `http://localhost:3020/${id}/reservations` where id is the the last / section of the url
-
-*/
